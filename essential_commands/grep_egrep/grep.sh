1a56f0460faaed4d7fcb8be2d20ca17513dc5063
Javier Galarza <jegj57@gmail.com>
2020-12-10 21:36:09 -0500

    more grep samples

--- essential_commands/grep_egrep/grep.sh
+++ essential_commands/grep_egrep/grep.sh
@@ -20,5 +20,15 @@ egrep  -i "Seinfeld|Bhatt" sandbox/seindfield
 # Regular expression
 grep '^foo$' sandbox/seindfield
 
+# using extended grep -E (for enhanced regular expressions)
+grep -E '^foo$' sandbox/seindfield
+
 # match digits
 grep 'foo[0-9][0-9]' sandbox/seindfield
+
+# Search pattern inside file in path/*. Show only file name
+grep -l Mario sandbox/testfolder/*
+egrep -li '^mario' sandbox/testfolder/*
+
+# Search pattern inside file in path/* RECURSIVE. Show only file name
+egrep -lir '^mario' sandbox/testfolder/*
