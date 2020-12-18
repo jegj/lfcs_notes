9bc70300d671d5b0439262b1024dff15333fcf76
Javier Galarza <jegj57@gmail.com>
2020-12-18 08:26:43 -0500

    Update sed samples

--- essential_commands/sed/sed.sh
+++ essential_commands/sed/sed.sh
@@ -6,27 +6,54 @@ sed 's/Dolores/Chicken/g' sandbox/test
 # Replace a word and update the file
 sed -i 's/Dolores/Chicken/g' sandbox/test
 
+# Save result to file but keep an copy of original file with name file.orign
+sed -i.orign 's/Dolores/Chicken/g' sandbox/test
+
+# In any row of file, it will change all occurrences of source to target. Ignore case = case insensitive. Print all rows
+sed 's/dolores/Chicken/gI'
+
+# For row 13, it will change first occurrence of source to target. Print all rows
+sed '13s/Dolores/TARGET/' sandbox/test
+
+# Replace word except by line 25
+sed '25!s/Dolores/s/g' sandbox/test
+
+# In any row of file, it will change first occurrence of source to target. Print only changed rows
+sed -n 's/Dolores/TARGET/p'
+
 # Replace with empty
 sed 's/Dolores//g' sandbox/test
-
-# Delete line that match word
-sed '/Dolores/d' sandbox/test
-
-# Delete empty lines that match word
-sed '/^$/d' sandbox/test
-
-# Delete by line
-sed '1d' sandbox/test # first line
-sed '1,3d' sandbox/test # 1 to 3
 
 # Replace tabs with spaces
 sed 's/\t/ /g' sandbox/test
 
-# Show lines
+
+######## DELETE
+
+# Delete line that match word
+sed '/Dolores/d' sandbox/test
+
+# Delete empty lines
+sed '/^$/d' sandbox/test
+
+# Delete by line number
+sed -n 12d sandbox/test # line 12 and no print new result
+sed '1d' sandbox/test # first line
+sed '1,3d' sandbox/test # 1 to 3
+sed 12,18d sandbox/test # 12 to 18
+
+######## ADD
+
+# It will insert newline as line 11
+sed '11inewline' sandbox/test
+
+########### PRINT
+
+# Show lines from 12 to 18
 sed -n 12,18p sandbox/test
 
-# Show Everything except lines - Delete lines
-sed 12,18d sandbox/test
+# In any row of file, it will change first occurrence of source to target. Print only changed rows
+sed -n 's/source/target/p'
 
-# Replace word except by line 25
-sed '25!s/Dolores/s/g' sandbox/test
+# It will print only rows that contain source
+sed -n '/source/p' file
