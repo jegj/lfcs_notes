7e872f1199d56ff9dc6e85e274d98ef951c1fb3a
Javier Galarza <jegj57@gmail.com>
2020-12-18 08:45:36 -0500

    update losetup sample

--- Disk Partitioning/sample.sh
+++ Disk Partitioning/sample.sh
@@ -2,8 +2,13 @@
 
 sudo dd if=/dev/zero of=/imagefile bs=1M count=250
 
-sudo mkfs -t ext4 -b 2048 -v imagefile
-sudo mkfs -t ext4 -b 4096 -v imagefile
+losetup -f
+sudo losetup /dev/loop<x>/ imagefile
+losetup -l
+
+sudo mkfs -t ext4 -b 1024 -v imagefile # blocksize 1024
+sudo mkfs -t ext4 -b 2048 -v imagefile # blocksize 2048
+sudo mkfs -t ext4 -b 4096 -v imagefile # blocksize 4096
 
 sudo mkdir /mnt/tempdir
 sudo mount -o loop /imagefile /mnt/tempdir$
