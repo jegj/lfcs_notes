d99f9b6197954eee5242e35ca74b383f93486769
Javier Galarza <jegj57@gmail.com>
2021-01-05 08:28:21 -0500

    Move section and add sysctl basics

---
+++ Operation of Running Systems/sysctl/init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# The sysctl interface can be used to read and tune kernel parameters at run time
+
+# 1) Show kernel parameters
+sysctl -a
+
+# 2) Edit kernel params with sysctl
+sudo sysctl kernel.pid_max=2000
+# or
+echo 3000 > /proc/sys/kernel/pid_max
+
+sysctl  net.ipv4.icmp_echo_ignore_all
+sysctl  net.ipv4.icmp_echo_ignore_all=1
+
+# 3) Make permanent
+echo "net.ipv4.icmp_echo_ignore_all=1" >> /etc/sysctl.conf
+sysctl -p
+
+# https://www.kernel.org/doc/Documentation/
