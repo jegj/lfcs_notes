8bc9b3ad348f8a69dcdec7ff2fe5835830a0aed2
Javier Galarza <jegj57@gmail.com>
2021-01-25 08:15:00 -0500

    Incompelte selinux and apparmor

---
+++ Operation of Running Systems/selinux&apparmor/apparmor.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt install apparmor apparmor-profiles apparmor-utils
+
+
+
+
+
+
+
+
+
+
+
---
+++ Operation of Running Systems/selinux&apparmor/selinux.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Install
+sudo apt install selinux selinux-utils selinux-basics auditd audispd-plugins
+
+# 1) Show SELinux state
+getenforce
+
+# 2) Define SELinux mode
+
+setenforce Permissive
+setenforce Enforcing
+
+
+
+# - https://www.linode.com/docs/guides/how-to-install-selinux-on-ubuntu-18-04/
