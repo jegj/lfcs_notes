df43d3b6a0117188a54a739b12bd4825bf339e10
Javier Galarza <jegj57@gmail.com>
2021-02-08 08:22:30 -0500

    IMprove reboot options

--- Operation of Running Systems/reboot/init.sh
+++ Operation of Running Systems/reboot/init.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-
-#1) Boot, reboot, and shut down a system safely
+# 1) Boot, reboot, and shut down a system safely
 sudo shutdown -h +1 "Power Failure imminent" # shutdown with message
 sudo shutdown -h now  #shutdown
+sudo shutdown -P now  #shutdown
 sudo shutdown -r now  #reboot
 sudo shutdown -r now 'Kernel upgrade requires reboot'
 sudo reboot
+
+# 2) with systemd
+systemctl poweroff
+systemctl reboot
+systemct
+
+# 3) Hard option
+
+echo b > /proc/sysrq-trigger
