d4aa2dc92e4c9e29ca35534935e6be017391d55e
Javier Galarza <jegj57@gmail.com>
2021-02-08 21:07:48 -0500

    Add dummy apparmor commands

--- Operation of Running Systems/selinux&apparmor/apparmor.sh
+++ Operation of Running Systems/selinux&apparmor/apparmor.sh
@@ -2,8 +2,16 @@
 
 sudo apt install apparmor apparmor-profiles apparmor-utils
 
+# Show current state
+aa-status
 
+# Create new profile
 
+cp /usr/bin/vim /usr/local/bin/kim
+aa-genprof kim
+
+# Enforce mode on kim
+aa-enforce /usr/local/bin/kim
 
 
 
