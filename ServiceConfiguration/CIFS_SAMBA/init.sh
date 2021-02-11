b3228d9bc2dfae9702a47672dff5e5c16038852b
Javier Galarza <jegj57@gmail.com>
2021-02-10 21:02:10 -0500

    samba demo

---
+++ ServiceConfiguration/CIFS_SAMBA/init.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Install samba
+apt install samba
+
+# Add data share /etc/samba/smb.conf
+[backups]
+   comment = backups
+   path = /backups
+   browseable = yes
+   writeable  = yes
+   valid users = linda
+
+# Restart Service
+systemctl restart smbd
+
+# Create smb user
+smbpasswd -a linda # test
+
+#### On client
+
+# Install dependecies
+apt install samba-client cifs-utils
+
+# List smb shares
+smbclient -L master
+
+# Mount
+mount -o username=linda,password=test //master/backups /mnt
+
+# Add entry fstab
+//master/backups          /backups                  cifs       _netdev,username=linda,password=test 0 0
