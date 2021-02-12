dc9176f46a94f610712ea9878fef6dde2feb6d35
Javier Galarza <jegj57@gmail.com>
2021-02-12 10:21:44 -0500

    Add run levels

--- ServiceConfiguration/systemd/runlevel.sh
+++ ServiceConfiguration/systemd/runlevel.sh
@@ -6,5 +6,12 @@ systemctl get-default
 # Set default run level
 systemctl set-default multi-user.target
 
+-poweroff.target => 0
+-rescue.target => 1
+-multi-user.target => 3
+-graphical.target => 5
+-reboot.target => 6
+-emergency.target
+
 # Switch run level
 systemctl isolate multi-user.target
