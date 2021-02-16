a6a016a0438c54f1eaa8cfa4cfd72d019a3c7b11
Javier Galarza <jegj57@gmail.com>
2021-02-16 08:28:50 -0500

    Add a new sample on systemd mount

--- ServiceConfiguration/systemd/mount.sh
+++ ServiceConfiguration/systemd/mount.sh
@@ -26,6 +26,23 @@ systemctl daemon-reload
 # Enable the mount and start it
 systemctl enable --now backups.mount
 
+### Network dependecies
+
+[Unit]
+Description=Samba dir
+Documentation=man:hier(7)
+Conflicts=umount.target
+After=network.target
+
+[Mount]
+What=//master/myfiles
+Where=mnt
+Type=cifs
+Options=_netdev,username=anna,password=12345,uid=vagrant,gid=vagrant
+
+[Install]
+WantedBy=local-fs.target
+
 #### AutoMount unit => /etc/systemd/system/backups.automount
 [Unit]
 Description=Automount backups
