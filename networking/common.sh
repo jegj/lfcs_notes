460a989a926fd4214884487b5b95976c71513220
Javier Galarza <jegj57@gmail.com>
2021-02-23 08:04:27 -0500

    Move to common networking

--- networking/common.sh
+++ networking/common.sh
@@ -6,6 +6,9 @@ ip a
 # Add a new ip
 ip a a dev eth0 1.2.3.4/8
 
+# Add static route
+ip route add 172.16.5.0/24 via 10.0.0.101 dev eth0
+
 # Hostname details
 hostnamectl status
 
--- networking/static_routing.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ip route add 172.16.5.0/24 via 10.0.0.101 dev eth0
