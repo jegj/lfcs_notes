8b92c1c769eef91c65be4b8801a2e90bdedbba88
Javier Galarza <jegj57@gmail.com>
2021-02-26 08:17:17 -0500

    improve email

--- ServiceConfiguration/email/init.sh
+++ ServiceConfiguration/email/init.sh
@@ -3,5 +3,36 @@
 # Install package
 apt install postfix
 
+# PORT: 25
+
 # Configure /etc/postfix
-TODO:
+
+# /etc/postfix/master.cf
+
+# /etc/postfix/main.cf
+
+inet_interfaces = all
+inet_protocols = ipv4
+myorigin = $mydomain
+
+systemctl restart postfix
+
+## dovecot mutt
+apt install dovecot mutt
+
+
+# /etc/dovecot/dovecot.conf
+protocols = imap pop4 lmtp
+listen = *, ::
+
+# /etc/dovecot/conf.d/10-mail.conf
+mail_location = ****
+
+systemctl restart dovecot
+systemctl enable dovecot
+
+# Add user to mail group
+usermod -aG mail linda
+
+# Connect to mutt
+mutt -f imap://linda@localhost
