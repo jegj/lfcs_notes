18a8f1dc80583fdf4bfda2c89ff609141efff180
Javier Galarza <jegj57@gmail.com>
2021-03-02 08:22:02 -0500

    Improve network samples

--- networking/common.sh
+++ networking/common.sh
@@ -6,6 +6,9 @@ ip a
 # Add a new ip
 ip address add dev eth0 1.2.3.4/8
 
+# Dele IP
+ip address del dev eth0 1.2.3.4/8
+
 # Add static route
 ip route add 172.16.5.0/24 via 10.0.0.101 dev eth0
 
@@ -18,6 +21,12 @@ hostnamectl set-hostname vagrant.test.com
 
 # Show the default gateway
 ip route show
+
+# Delete default route
+ip route del default via 10.0.2.2
+
+# Add default route
+ip route add default via 10.0.2.2
 
 # Renew ip
 dhclient
@@ -35,5 +44,40 @@ nmap localhost
 dig nu.nl
   # dns file ==> /etc/resolv.conf
 
+##### NETPLAN #####
+
 # /etc/netplan/01-netcfg.yaml
 sudo nano /etc/netplan/01-netcfg.yaml
+
+
+####### SYSTEMD ############
+
+# Install packages
+apt install systemd-networkd systemd-resolved
+
+# Remove network manager
+systemctl disable --now NetworkManager
+
+# Enable services
+systemctl enable systemd-networkd
+systemctl enable systemd-resolved
+
+# Delete
+rm /etc/resolv.conf
+
+# Create symb link
+ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
+
+# Create config folder
+mkdir /etc/systemd/network
+
+# Create config
+vim 10-static-ens33.network
+
+[Match]
+Name=ens33
+
+[Network]
+address=192.168.4.229/24
+Gateway=192.168.4.2
+DNS=127.0.0.1
